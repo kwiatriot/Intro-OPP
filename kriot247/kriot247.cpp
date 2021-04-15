6c8b2503376e0610fc78f9e802e0d741ee9d934c
Wayne Kwiat Jr <wkwiatjr@me.com>
2021-04-15 16:15:33 -0600

    Using templates and exception handling

--- kriot247/kriot247.cpp
+++ kriot247/kriot247.cpp
@@ -2,30 +2,65 @@
 Date: 4/8/21
 Author: Wayne Kwiat
 Description:
-Create a pure abstract class and two derived classes. 
-After you have created these classes, create a driver program that defines a Circle object and a Rectangle object. 
-Demonstrate that each object properly calculates and reports its area.
+Write a class named TestScores. The class constructor should accept an array of test scores as its argument. 
+The class should have a member function that returns the average of the test scores. 
+If any test score in the array is negative or greater than 100, the class should throw an exception. 
+Demonstrate the class in a program.
 */
 
 
 #include <iostream>
-#include "BasicShape.h"
-#include "Circle.h"
-#include "Rectangle.h"
+
 using namespace std;
+
+template <class T>
+class TestScores 
+{
+private:
+    T arr[5];
+
+public:
+    class exp{};
+    // Constructor
+    TestScores(T y[]) 
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            if (y[i] < 0 || y[i] > 100)
+                throw exp{};
+            else
+                arr[i] = y[i];
+        }
+    }
+    T avg()
+    {
+        T total = 0;
+        for (int i = 0; i < 5; i++)
+            total = total + arr[i];
+        return total / 5.0;
+    }
+
+};
 
 int main()
 {
-    Circle cir1(0, 0, 4);
-    Rectangle rec1(8, 12);
-
-    cir1.calcArea();
-    rec1.calcArea();
-
-    cout << "The circle's area is: " << cir1.getArea() << endl;
-    cout << "The rectangle's area is: " << rec1.getArea() << endl;
-
+    int a[5] = { 3, 6, 2, 9, 1 };
+    double b[5] = { 3.6, 2.8, 7.1, 8.4, 9.7 };
+    try {
+    TestScores <int>o1(a);
+    cout << "This is the average of the integer array: " << o1.avg() << endl;
+    }
+    catch (TestScores<int>::exp)
+    {
+        cout << "One or more numbers is below 0 or greater then 100" << endl;
+    }
+    try {
+        TestScores <double>o2(b);
+        cout << "This is the average of the double array: " << o2.avg() << endl;
+    }
+    catch (TestScores<double>::exp)
+    {
+        cout << "One or more numbers is below 0 or greater then 100" << endl;
+    }
     return 0;
-
 }
-
